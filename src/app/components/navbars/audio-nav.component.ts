d8434f18f633094a24f24b4b0aff0b38f5dea465
Marisa <depasquale.marisa@gmail.com>
2017-12-08 09:49:18 -0500

    minor things

--- src/app/components/navbars/audio-nav.component.ts
+++ src/app/components/navbars/audio-nav.component.ts
@@ -142,6 +142,8 @@ export class AudioNavComponent implements OnInit {
     this.audioService.songQueue.subscribe((songQueue) => {
       this.songQueue = songQueue;
       this.player = this.createHowl(songQueue[this.songQueueIndex]);
+      this.playing = false;
+      this.togglePlay();
     });
     // this.songQueue = [];
     this.shuffle = false;
