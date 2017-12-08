d338006a8e4de42ac232c1bd0defb377bd74a621
Mrunal Patel <mkpatel@cs.stonybrook.edu>
2017-12-07 21:03:28 -0500

    added audio

---
+++ src/app/services/audio/audio.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import 'howler';
+import {BehaviorSubject} from "rxjs/BehaviorSubject";
+
+@Injectable()
+export class AudioService {
+
+  private queueSource = new BehaviorSubject<Array<Howl>>([]);
+  public songQueue = this.queueSource.asObservable();
+
+  constructor() { }
+
+  setQueue(songs: Array<Howl>) {
+    this.queueSource.next(songs);
+  }
+
+}
